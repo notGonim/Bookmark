89f56108e1fa5d0e28646930ef4d63cd540b4f61
notGonim <mhmodgonim96@gmail.com>
2021-07-18 12:22:35 +0200

    ADDED Spinner component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router";
 import { lazy, Suspense } from "react";
 import * as ROUTES from './constants/routes'
 
+import IsUserLoggedIn from "./helpers/isUserLoggedIn";
+import ProtectedRoute from "./helpers/protectedRoutes";
 
 
 
---
+++ src/components/spinner/spinner.component.js
@@ -0,0 +1,22 @@
+import React from "react";
+import '../../App.css';
+
+function Spinner() {
+    return (
+        <>
+            <svg className="spinner" viewBox="0 0 50 50">
+                <circle
+                    className="path"
+                    cx="25"
+                    cy="25"
+                    r="20"
+                    fill="none"
+                    stroke-width="5"
+                ></circle>
+            </svg>
+        </>
+    );
+}
+
+export default Spinner;
+
