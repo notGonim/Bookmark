b8ac7e2860dacc869dec741fdde4b44babe62392
notGonim <mhmodgonim96@gmail.com>
2021-07-28 06:39:27 +0200

    Added the firebase && users context

---
+++ src/store/firebase/firebase.context.js
@@ -0,0 +1,5 @@
+import { createContext } from 'react'
+
+//create Firebase context
+const FirebaseContext = createContext(null)
+export default FirebaseContext
---
+++ src/store/users/user.context.js
@@ -0,0 +1,6 @@
+import { createContext } from 'react';
+
+
+//create User context
+const UserContext = createContext(null);
+export default UserContext
